cbc9c7e9faf69de7a1e6e1c64de2dd1d8915e346
Guilherme Henrique <gh.dourado77@gmail.com>
2023-07-31 13:25:17 -0300

    Banner Sobre

--- src/components/SobreNos/BannerSobre.jsx
+++ src/components/SobreNos/BannerSobre.jsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
-import IphoneImage from "../../assets/iphoneImage.png";
+import ImagePhone from '../../assets/iphoneImage.png'
 
 export function BannerSobre() {
   return (
-    <div className="bg-gradient-to-r from-[#ee6c09] to-orange-300 w-full">
-      <div className="md:h-[600px] grid gap-12 md:grid-cols-2 md:px-32">
-        <div className="text-black flex flex-col justify-center space-y-4 px-2">
-          <h2 className="font-medium text-sm sm:text-base pt-12 text-white">
-            Descubra o Futuro da Tecnologia em Nossos Celulares!
+    <div className="bg-[url('/bgPhone.webp')] bg-cover w-full">
+      <div className="md:h-[600px] bg-black bg-opacity-90 grid gap-12 md:grid-cols-2 md:px-32">
+        <div className="text-white flex flex-col justify-center space-y-4 px-2">
+          <h2 className="font-medium text-sm sm:text-base pt-12 text-orange-500">
+            Descubra o Futuro da Tecnologia em <br />Nossos Celulares!
           </h2>
           <h1 className="text-3xl sm:text-5xl font-bold">
-            <span>Bem-vindo</span> à nossa loja, onde a inovação encontra a praticidade.
+            <span>Bem-vindo</span> à nossa loja, onde a inovação encontra a
+            praticidade.
           </h1>
         </div>
         <div className="flex items-center justify-center">
-          <Image src={IphoneImage} alt="Banner" className="w-full" />
+          <Image
+            src={ImagePhone}
+            alt="Banner"
+            className="w-full"
+          />
         </div>
       </div>
     </div>
--- src/components/SobreNos/SobreMim.jsx
+++ src/components/SobreNos/SobreMim.jsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
-import ImageBanner from "../../assets/banner.png";
 
 export function SobreMim() {
   return (
     <div>
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 items-center gap-8 py-12 px-2">
-        <Image src={ImageBanner} alt="Imagem da Loja" />
+        <Image
+          width={500}
+          height={500}
+          src={"/banner2.jpg"}
+          alt="Imagem da Loja"
+        />
         <div className="sm:text-lg space-y-4 text-center">
-          <h2 className="text-2xl sm:text-4xl font-bold">
-            Quem Somos
-          </h2>
+          <h2 className="text-2xl sm:text-4xl font-bold">Quem Somos</h2>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
             aspernatur debitis quibusdam, cum similique ea voluptas quis eaque
